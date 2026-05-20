8137318187a5b9efd93108800730445262797a1e
Sinuhe Maceda <sinuhe.dev@gmail.com>
2026-05-20 04:08:02 -0600

    updated

---
+++ src/bin.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+/**
+ * Copyright (c) 2026 Sinuhe Maceda https://sinuhe.dev
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * https://github.com/sinuhedev/nextia/create-nextia
+ */
+
+console.info("create-nextia");
